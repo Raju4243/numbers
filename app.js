86f1be5a9defda6eaadfd848fc2e62d5707b11d7
Raju4243 <raju.ee.1723@gmail.com>
2021-12-04 06:45:00 +0600

    numbers count

---
+++ app.js
@@ -0,0 +1,9 @@
+var a=1;
+function increment(){
+    document.getElementById("container").innerHTML=a;
+    a=a+1;
+}
+function decrement(){
+    document.getElementById("container").innerHTML=a;
+    a=a-1;
+}
